940b3613216d7913bb482ec57dd3b7fffdcf0378
0KeirLi0 <158504174+0KeirLi0@users.noreply.github.com>
2025-09-02 02:32:48 +0800

    update app.py

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
 from src.app.layout import get_layout
 
+app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
+server = app.server
 
 def main():
-    plotly_could = True
-    app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
     app.title = "Keir's Charts Collection"
     app.layout = get_layout()
-    if plotly_could:
-        server = app.server
-        app.run()
-    else:
-        app.run(debug=True, host='127.0.0.1', port=8050)
+    app.run()
+    # app.run(debug=True, host='127.0.0.1', port=8050)
     
     
 
